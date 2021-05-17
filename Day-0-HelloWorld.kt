04daff278115fd22500a464ba5efff97cd519946
Zaccheaus Amenya <zamenya98@gmail.com>
2021-05-17 09:28:34 +0300

    Create Day 0. Hello world

---
+++ Day-0-HelloWorld.kt
@@ -0,0 +1,13 @@
+import java.io.*
+import java.util.*
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
+        
+      val inputString = readLine()
+        
+        println ("Hello, World.")
+        
+        println(inputString)
+}
