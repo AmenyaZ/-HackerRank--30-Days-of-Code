69ca83677b82eae44ddb355ea43c9a46d9719442
Zaccheaus Amenya <zamenya98@gmail.com>
2021-05-29 14:47:31 +0300

    Day 10: Binary Numbers

---
+++ Day 10: Binary Numbers.kt
@@ -0,0 +1,35 @@
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+    
+
+    var div:Int = n
+    var sequence = 0
+    var mod:Int
+    var max1InSequence = 0
+    var previous:Int = -1
+
+    do {
+        mod = div%2
+
+        if (previous == -1){
+            previous = mod
+        }
+
+        if (mod == 1) {
+            sequence++
+        }
+
+        div /= 2
+
+        if((mod != 1 && previous == 1) || div <= 0){
+            if(sequence > max1InSequence) {
+                max1InSequence = sequence
+            }
+            sequence = 0
+        }
+
+        previous = mod
+    } while(div > 0)
+
+    println(max1InSequence)
+}        
